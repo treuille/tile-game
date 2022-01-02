394df5450cded2d546c39fcf6c91fc08c7c63fc1
Adrien Treuille <adrien.g.treuille@gmail.com>
2022-01-01 17:03:31 -0800

    Starting to work on pariwise permute logic.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,42 @@ def main():
     print(board)
     print(board.dtype)
 
-    permute(board)
+    slide_iter(board)
 
 
-def permute(board):
+def slide_iter(board):
     w, h = board.shape
     space = np.array(np.where(board == 0)).T[0]
     print(space)
     print(board[tuple(space)])
     print(DIRECTIONS)
+    adjacencies = space + DIRECTIONS
+    print("adj", adjacencies)
+    in_bounds = np.logical_and.reduce(
+        [
+            adjacencies[:, 0] >= 0,
+            adjacencies[:, 0] < w,
+            adjacencies[:, 1] >= 0,
+            adjacencies[:, 1] < h,
+        ]
+    )
+    print("in_bounds", in_bounds)
+    adjacencies = adjacencies[in_bounds]
+    print("adj", adjacencies)
+    # adjacent_tiles = board[adjacencies[..., 0], adjacencies[..., 1]]
+    # print("tiles", adjacent_tiles)
+    print("perm", permute(board, (0, 0), (0, 1)))
+
+
+def permute(board, pt_1, pt_2):
+    pt_1, pt_2 = tuple(pt_1), tuple(pt_2)
+    board_2 = np.array(board)
+    # print(board, id(board))
+    # print(board_2, id(board_2))
+    # print(pt_1, pt_2)
+    board_2[pt_1] = board[pt_2]
+    board_2[pt_2] = board[pt_1]
+    return board_2
 
 
 if __name__ == "__main__":
